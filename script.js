fe6958e0cbf1c3ba709fbed9437d83218b7658f0
Thili007 <tpunchihewage@gmail.com>
2022-09-22 14:28:31 +0200

    added files

---
+++ script.js
@@ -0,0 +1,22 @@
+const cite = document.querySelector("cite").nodeName;
+const footer = document.querySelector("cite").parentElement.nodeName;
+const blockquote = document.querySelector(footer).parentElement.nodeName;
+const section = document.querySelector(blockquote).parentElement.nodeName;
+const main = document.querySelector(section).parentElement.nodeName;
+const body =
+  document.querySelector(section).parentElement.parentElement.nodeName;
+const html =
+  document.querySelector(section).parentElement.parentElement.parentElement
+    .nodeName;
+
+const arr = [];
+let currentElement = document.querySelector("cite");
+
+while (currentElement) {
+  let currentElementName = currentElement.nodeName;
+  arr.unshift(currentElementName.toLowerCase());
+
+  currentElement = currentElement.parentElement;
+}
+
+console.log(arr.join(" >-> "));
